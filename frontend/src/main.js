715f66727b14ba32839f84b802f5691a2fdd1bf2
Evaristo Ramalho <erlds5518@gmail.com>
2022-01-22 10:10:18 -0300

    Home Component

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,6 +9,9 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 
+//TEMPORARIO!
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkV2YXJpc3RvIFJhbWFsaG8iLCJlbWFpbCI6ImVybGRzNTUxOEBnbWFpbC5jb20iLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNjQyODU2MDc3LCJleHAiOjE2NDMxMTUyNzd9.hUaV1C55bdp8yjhHDbhBKDjJl9ObDtC7qZHUUsAIPu4'
+
 new Vue({
   store,
   router,
